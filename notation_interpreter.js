25ebcfd1b7a2798bc8b3a3351b8f2c6bcd182e64
Jack Kilrain <u6940136@anu.edu.au>
2019-05-09 11:30:44 +1000

    Fixed type evaluation in evalBraKet()

--- notation_interpreter.js
+++ notation_interpreter.js
@@ -13,15 +13,14 @@ const one = [0,1];
 module.exports = {
 
   evalBraKet: function evalBraKet(expression) {
-    var val = new String(expression);
-    if (!(val instanceof String)) {
+    if (typeof expression !== "string") {
       throw new Error(`Error: Expression must be of type String`);
     } else {
       var eval_exp = new Array();
-      for (var i in val) {
-        if (val.charAt(i) === '0') {
+      for (var i in expression) {
+        if (expression.charAt(i) === '0') {
           eval_exp.push(zero);
-        } else if (val.charAt(i) === '1') {
+        } else if (expression.charAt(i) === '1') {
           eval_exp.push(one);
         }
       }
