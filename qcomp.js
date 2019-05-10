b2f00b3f13e0efd582a25ea890a75da72b903469
Jack Kilrain <u6940136@anu.edu.au>
2019-05-10 21:21:57 +1000

    Added library import for qubit_formatter.js

--- qcomp.js
+++ qcomp.js
@@ -7,9 +7,10 @@
 * Description: Qubit representation and assosciated logic gates
 */
 
-// Include the MathJs library
+// Library imports
 const math = require('mathjs');
-const inter = require('./notation_interpreter.js')
+const inter = require('./notation_interpreter.js');
+const form = require('./qubit_formatter.js');
 
 const posi = math.complex(0, 1);
 const negi = math.complex('-i');
@@ -88,7 +89,7 @@ class QC {
   * q(n) -> |000...00>
   */
   qreg(registerCount) {
-    this.value = nKron(Array(registerCount).fill(zero));
+    this.value = form.nKron(Array(registerCount).fill(zero));
     console.log("Initial value: [" + this.value + "]")
     this.amplitudes = [];
     return this;
@@ -300,81 +301,9 @@ class QC {
 
 }
 
-function entangle(qubit) {
-  var p1_amps = [];
-  var p2_amps = [];
-  const comb_particles = nKron(qubits.value);
-
-  comb_particles.forEach(function(i) {
-    if (i == zero) {
-      p1_amps.push(qubit.values[i]);
-    } else if (i == one) {
-      p2_amps.push(qubit.values[i]);
-    } else {
-      continue;
-    }
-  })
-  return p1_amps + p2_amps;
-}
-
-// Collapse a superposition to display or directly display a qubit(s) value
-function measure(qubit) {
-  var amp_size = qubit.amplitudes.length;
-  if (amp_size > 0) {
-    // If there is a superposition, collpase it and return the results and probabilities
-    var prob = Math.floor(Math.random() * amp_size)
-    return "\nCollapsed State: " + qubit.amplitudes[prob][0] + "\nProbability: " + qubit.amplitudes[prob][1] + "\n";
-  } else {
-    // If there is no superposition, return the results
-    return qubit.value;
-  }
-}
-
-// Return the stored qubit value
-function displayValue(qubit) {
-  return qubit.value;
-}
-
-// Create a multi-qubit matrix (nKron qubits)
-function nKron(arglist) {
-  if (arglist.length == 1) {
-    // If there is only one value then return it
-    return arglist[0]
-  } else {
-    // Generate the identity matrix that matches the arglist length
-    var value = math.identity(2 ** arglist.length);
-    var str_val_line = "";
-    arglist.forEach(function(i) {
-      if (i.equals(zero) === true) {
-        str_val_line += "0";
-      } else if (i.equals(one) === true) {
-        str_val_line += "1";
-      }
-    })
-    // Return the row in the identity matrix that matches the combined argument list
-    return value._data[parseInt(str_val_line, 2)]
-  }
-}
-
-// Return a pattern of one and zero vectors that match the nKron value
-function unKron(arglist) {
-  // Find the index of the 1
-  var index_value = arglist.indexOf(1);
-  var str_val_line = "";
-  // Check if there are zeros before the one and add them
-  if (index_value < Math.log2(arglist.length)) {
-    for (var i = 0; i < Math.log2(arglist.length) - index_value; i++) {
-      str_val_line += "0";
-    }
-  }
-  str_val_line += index_value.toString(2);
-  // Return the notation evaluated string
-  return inter.evalBraKet(str_val_line);
-}
-
 var qc = new QC();
 var newBit = qc.qreg(2);
 
-console.log(unKron(nKron(inter.evalBraKet("|100>"))));
-console.log(nKron(inter.evalBraKet("|01>")));
-console.log(displayValue(qc.cx(qc.h(newBit))));
+console.log(form.unKron(form.nKron(inter.evalBraKet("|100>"))));
+console.log(form.nKron(inter.evalBraKet("|01>")));
+console.log(form.displayValue(qc.cx(qc.h(newBit))));
